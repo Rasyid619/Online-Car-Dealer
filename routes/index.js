ac4f60511e0044430ac1f2954bb58d9fafec6f87
Rasyid619 <arifinrasyid13@gmail.com>
2022-02-24 19:44:35 +0700

    tambahan

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
 const Controller = require('../controllers/controllerCarDealers')
+const ControllerUser = require('../controllers/controllerUserProfile');
 const routerCarDealers = require('./routerCarDealers')
 const routerUserProfile = require('./routerUserProfile')
 
 
 
 router.get('/', Controller.getHome)
+router.get('/register', ControllerUser.getRegisterForm)
+router.post('/register', ControllerUser.postRegisterForm)
 router.get('/login', Controller.getLogin)
 router.post('/login', Controller.postLogin)
 
-
+router.use((req, res, next) => {
+  console.log(req.session)
+  next()
+})
 
 router.use('/car', routerCarDealers)
 router.use('/users', routerUserProfile)
